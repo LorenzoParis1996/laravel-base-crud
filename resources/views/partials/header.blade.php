b292189b300f1d0319387dbca9639989dface785
LorezoParis1996 <lorenzoparis86@live.it>
2024-07-23 16:53:47 +0200

    aggiunti link per navigare tra form e index

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,6 @@
 <h1 class="text-center">Riserva Naturale</h1>
+<div class="d-flex justify-content-center">
+    <h3 class="me-2"><a href="/create" class="link-underline link-underline-opacity-0">Crea un nuovo animale</a></h3>
+    <h3 class="ms-2"><a href="/" class="link-underline link-underline-opacity-0">Torna alla lista di animali</a></h3>
+</div>
+
